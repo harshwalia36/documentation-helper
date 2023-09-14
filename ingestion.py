e4a5d1b84ad53064f07a3abbe7b12abad8060c35
harshwalia36 <harsh.p.walia@gmail.com>
2023-09-14 20:11:42 +0530

    implement ingestion.py

---
+++ ingestion.py
@@ -0,0 +1,43 @@
+import os
+import getpass
+
+from langchain.document_loaders import ReadTheDocsLoader,RecursiveUrlLoader
+from langchain.text_splitter import RecursiveCharacterTextSplitter
+# from langchain.embeddings import OpenAIEmbeddings
+from langchain.embeddings import HuggingFaceEmbeddings
+from langchain.vectorstores import Pinecone
+import pinecone
+from langchain.vectorstores import FAISS
+
+
+OPENAI_API_KEY = os.environ.get("OPENAI_API_KEY")
+HUGGINGFACEHUB_API_TOKEN = os.environ.get("HUGGINGFACEHUB_API_TOKEN")
+
+# Uncomment the following line if you need to initialize FAISS with no AVX2 optimization
+# os.environ['FAISS_NO_AVX2'] = '1'
+
+
+# pinecone.init(
+#     api_key=os.environ["PINECONE_API_KEY"],
+#     environment=os.environ["PINECONE_ENVIRONMENT_REGION"],
+# )
+
+def ingest_docs()->None:
+    loader=RecursiveUrlLoader(url='https://python.langchain.com/docs/modules/')
+    raw_documents=loader.load()
+    print(f'loaded {len(raw_documents)} documents')
+    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000,chunk_overlap=100,separators=["\n\n","\n"," ",""])
+    documents=text_splitter.split_documents(documents=raw_documents)
+    print(f'Splitted into {len(documents)} chunks')
+
+    embeddings = HuggingFaceEmbeddings()
+    db = FAISS.from_documents(
+        documents, embeddings
+    )
+    print("****** Added to FAISS vectorstore vectors")
+
+    db.save_local("doc_helper_faiss_index")
+    
+
+if __name__ =='__main__':
+    ingest_docs()
